952441deb0f9ce92b37967ac215fd9906b98c8da
gonefishing4422 <119458070+gonefishing4422@users.noreply.github.com>
2023-05-24 14:40:05 -0500

    Update index and listing portion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,30 @@
-var faq = document.getElementsByClassName("faq-page");
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:16,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2500,
+    autoplayHoverPause:true,
+    animateOut:'slideOutDown',
+    animateIn:'flipInX',
+    smartSpeed:600,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+var faq=document.getElementsByClassName("faq-page");
 var i;
-for (i = 0; i < faq.length; i++) {
+for (i=0;i<faq.length;i++) {
     faq[i].addEventListener("click", function () {
-        var body = this.nextElementSibling;
+        var body=this.nextElementSibling;
         this.classList.toggle("active");
         body.classList.toggle("active");
     });
@@ -10,4 +32,3 @@ for (i = 0; i < faq.length; i++) {
 $('.toggle-bar').click(function(){
     $('.nav-right').slideToggle();
 });
-AOS.init();
--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,18 +1,18 @@
-(function() {
+(function(){
   var method;
-  var noop = function () {};
-  var methods = [
+  var noop=function () {};
+  var methods=[
     'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
     'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
     'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
     'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
   ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
-  while (length--) {
-    method = methods[length];
-    if (!console[method]) {
-      console[method] = noop;
+  var length=methods.length;
+  var console=(window.console=window.console||{});
+  while(length--){
+    method=methods[length];
+    if(!console[method]){
+      console[method]=noop;
     }
   }
 }());
